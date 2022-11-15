419718d95e547c296e648efe14dadbf4b4485b00
Hugo Sullivan <81408574+hugo-sullivan@users.noreply.github.com>
2022-11-15 16:10:25 +1100

    Removed comments from snmp.py

--- snmp.py
+++ snmp.py
@@ -1,38 +1,7 @@
 from pysnmp import hlapi
 from scanInterface import ScannerForm
 
-"""
-def main():
-    #args = sys.argv[1:]
-    #IP = input("Enter the desired IP address: ")
-    IP = "192.168.1.234"
-    OID = []    
-    OID_input = input("Enter the desired OID: ")
-    outputs = ["SNMP outputs"]
-    
-    if (IP.split()[0] == "-f"):
-        #python open file then loop through the for loops
-        f = open(IP.split()[1], "r")
-        IP_file  = f.read().split()
-        for target in IP_file:
-            outputs.append(get(target, OID, hlapi.CommunityData('public')))
-            print(target)
-	
-    else:
 
-    if (OID_input.split()[0] == "-b"):
-        bulk_non_repeaters = int(input("Enter the desired no repeater value: "))
-        bulk_max_repeaters = int(input("Enter the desired max repeater value: "))
-        OID.append(OID_input.split()[1])
-        outputs.append(get_bulk(IP,OID,hlapi.CommunityData('public'),bulk_max_repeaters,bulk_non_repeaters))
-    else:
-        OID.append(OID_input)
-        outputs.append(get(IP, OID, hlapi.CommunityData('public')))
-	
-    for response in outputs:
-        print(response)
-        print("\n")
-"""
 class Snmp(ScannerForm):
 
     def __init__(self, target, parameters):
