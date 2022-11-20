ecaaa03460da377425cb9c8177a0f850da778059
Hugo Sullivan <81408574+hugo-sullivan@users.noreply.github.com>
2022-11-20 22:38:20 +1100

    Delete scanTranslator.py

--- scanTranslator.py
+++
@@ -1,9 +0,0 @@
-from snmp import *
-
-def getScanner(scanType, target, parameters):
-    if (scanType == "snmp"):
-        return Snmp(target,parameters)
-    else:
-        print("Invalid scan type")
-        return 
-	
